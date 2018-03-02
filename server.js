f746041d0e1318b4190d62a3b4d5e24587039f90
Jacob Kahlbaugh <kahlb010@d.umn.edu>
2018-03-02 15:41:29 -0600

    fixed path issues

--- server.js
+++ server.js
@@ -24,14 +24,14 @@ app.use(body_parser.urlencoded({
 app.use(body_parser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://'+(require('my-local-ip')())+':4000/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://'+(require('my-local-ip')())+':'+port+'/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
-app.use(express.static(path.join(__dirname, '../../p5Projects/Snake\ Game/')));
+app.use(express.static(path.join(__dirname, '../../p5Projects/SnakeGame/')));
 
 app.get('/getSession', (req, res) => {
   // console.log('GET');
